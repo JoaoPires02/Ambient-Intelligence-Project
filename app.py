c3fae9caffd217d4a69a6362471016bcc6f563e8
miguelgago13 <mjlbgago@gmail.com>
2024-03-15 21:44:32 +0000

    initial attendance list implementation

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, render_template, send_from_directory, redirect, url_for
+from flask import Flask, request, render_template, send_from_directory, redirect, url_for, jsonify
 import os
+import classroom_controller
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,6 +16,22 @@ def room123student():
 def room123teacher():
     return render_template('room123teacher.html')
 
+@app.route('/get-attendance-list')
+def get_attendance_list():
+    names = list(classroom_controller.students.values()) 
+    return jsonify({'students': names})
+
+@app.route('/get-presences')
+def get_presences():
+    presences = list(classroom_controller.student_present)
+    presences_str = [] 
+    for i in presences:
+        if i == False:
+            presences_str += ["Missing"]
+        elif i == True:
+            presences_str += ["Present"]
+    return jsonify({'presences': presences_str})
+
 
 @app.route('/favicon.ico')
 def favicon():
@@ -24,7 +42,7 @@ def submit_codes():
     classroom_code = request.form['classroom_code']
     school_code = request.form['school_code']
     print(classroom_code)
-    print(school_code)    
+    print(school_code)
     if classroom_code == '123':
         if school_code == '123':
             return redirect(url_for('room123student'))
