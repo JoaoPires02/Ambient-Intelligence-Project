2e36df3e91edc7fdca422c134d6bd3e6ee0cfe36
JoaoPires02 <piresjoao2002@gmail.com>
2024-03-16 20:17:01 +0000

    implement light functionality

--- classroom_controller.py
+++ classroom_controller.py
@@ -19,53 +19,66 @@ GPIO.setup(LIGHT_IO[0], GPIO.OUT)
 GPIO.setup(LIGHT_IO[1], GPIO.OUT)
 GPIO.setup(LIGHT_IO[2], GPIO.OUT)
 GPIO.setup(PROJ_IO, GPIO.OUT)
+led0 = GPIO.PWM(LIGHT_IO[0], 1000)
+led1 = GPIO.PWM(LIGHT_IO[1], 1000)
+led2 = GPIO.PWM(LIGHT_IO[2], 1000)
+leds = [led0, led1, led2]
 
 ser = serial.Serial('/dev/ttyACM0')
 
 students = {'99090':'João Pires',
-            '99104':'Marco Castro aKa Deus do Técnico',
+            '99104':'Marco Castro',
             '99112':'Miguel Gago',
-            '96447':'Matilde Martins',
-            '96308':'Ricardo Pinto',
             '99217':'Francesco Pelizzari'}
 
-present_students = [] #Only IDs
+student_present = [False, False, False, False] #Only IDs
 
 sim_temp = 20
 ideal_temp = 22
 current_temp = [0, 0]
 current_light_level = [0, 0, 0]
-student_present = [False, False, False, False]
+student_seated = [False, False, False, False]
 
-def new_student(id):
-    if id not in students:
-        print('Error: you don\'t belong here')
+automatic_lights = True
+light_on = [False, False, True]
+button_pressed = False
+
+def get_light_intensity(n):
+    brightness = current_light_level[n]
+    if brightness < 200:
+        return 100
+    elif brightness >= 200 and brightness < 300:
+        return 80
+    elif brightness >= 300 and brightness < 400:
+        return 50
+    elif brightness >= 400 and brightness < 500:
+        return 30
     else:
-        present_students.append(id)
-
-def student_list():
-    res = 'List of students:\n'
-    for student in students:
-        res += student + ': ' + students[student]
-        if student in present_students:
-            res += ' - Present\n'
-        else:
-            res += ' - Missing\n'
+        return 0
 
 def get_current_temp():
     return sum(current_temp) / len(current_temp)
 
 def change_temp(t):
     global ideal_temp
+    global automatic_lights
     ideal_temp = float(format(t, '.1f'))
 
 def set_light(light_n, state):
-    print('Light number ' + str(light_n) + ' turned ', end='')
-    if state: print('on')
-    else: print('off')
+    global automatic_lights
 
-    if state: GPIO.output(LIGHT_IO[light_n], GPIO.HIGH)
-    else: GPIO.output(LIGHT_IO[light_n], GPIO.LOW)
+    if light_n == 9: # Number reserved to switch between manual and auto mode
+        automatic_lights = state
+
+    else:
+        print('Light number ' + str(light_n) + ' turned ', end='')
+        if state: print('on')
+        else: print('off')
+
+        global leds
+        automatic_lights = False
+        if state: leds[light_n].start(100)
+        else: leds[light_n].start(0)
 
 def set_proj(state):
     print('Projector turned ', end='')
@@ -119,8 +132,10 @@ def sensor_commands():
                 #print('LIGHT' + str(command[2]) + ' = ' + str(current_light_level[command[2]]))
 
             elif command[0] == BUTTON:
-                student_present[command[1]] = not(student_present[command[1]])
-                #print('STUDENT' + str(command[1]) + '_PRESENT = ' + str(student_present[command[1]]))
+                student_seated[command[1]] = not(student_seated[command[1]])
+                #print('STUDENT' + str(command[1]) + '_PRESENT = ' + str(student_seated[command[1]]))
+                global button_pressed
+                button_pressed = True
 
 def update_sim_temp():
     while True:
@@ -147,14 +162,52 @@ def lcd_manager():
     finally:
         lcd.close_lcd()
 
+def light_control():
+    global button_pressed
+    global light_on
+    prev_light_intensities = [0, 0, 0]
+    while True:
+        if automatic_lights:
+            light_intensities = [get_light_intensity(0), get_light_intensity(1), get_light_intensity(2)]
+            if button_pressed:
+                button_pressed = False
+                if student_seated[0] or student_seated[1]:
+                    led0.start(light_intensities[0])
+                    light_on[0] = True
+                else:
+                    led0.start(0)
+                    light_on[0] = False
+
+                if student_seated[2] or student_seated[3]:
+                    led1.start(light_intensities[1])
+                    light_on[1] = True
+                else:
+                    led1.start(0)
+                    light_on[1] = False
+                
+            if light_intensities != prev_light_intensities:
+                if light_on[0]:
+                    led0.start(light_intensities[0])
+                if light_on[1]:
+                    led1.start(light_intensities[1])
+                if light_on[2]:
+                    led2.start(light_intensities[2])
+            prev_light_intensities = light_intensities
+        
+        else:
+            pass
+            
+
 commands_thread = threading.Thread(target=manual_commands, name='Thread 1')
 sensors_thread = threading.Thread(target=sensor_commands, name='Thread 2')
 update_temp_thread = threading.Thread(target=update_sim_temp, name='Thread 3')
 lcd_thread = threading.Thread(target=lcd_manager, name='Thread 4')
+light_control_thread = threading.Thread(target=light_control, name='Thread 5')
 
 commands_thread.start()
 sensors_thread.start()
 update_temp_thread.start()
 lcd_thread.start()
+light_control_thread.start()
 
     
